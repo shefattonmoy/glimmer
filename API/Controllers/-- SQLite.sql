dbb0289498379eaa589656d7264af559be94d8cb
shefattonmoy <shefathossain7@gmail.com>
2024-03-15 03:47:24 +0600

    ssl done

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,15 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Kevin");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Jonathan");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Sarah");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Miller");
+
+INSERT INTO Users (Id, UserName)
+VALUES (5, "Peter");
