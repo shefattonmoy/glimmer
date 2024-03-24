997593ebdf2be8cb58391a192e93a53d0660b791
shefattonmoy <shefathossain7@gmail.com>
2024-03-24 22:35:36 +0600

    card done

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,18 @@
+import { Photo } from "./photo";
+
+export interface Member {
+    id: number;
+    userName: string;
+    photoUrl: string;
+    age: number;
+    knownAs: string;
+    created: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    interestedIn: string;
+    hobbiesAndInterests: string;
+    city: string;
+    country: string;
+    photos: Photo[];
+  }
