4d04ec8aec94c4433467d7790a532018971181d3
shefattonmoy <shefathossain7@gmail.com>
2024-04-17 22:15:39 +0600

    photo functionalities implemented

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -49,7 +49,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => 
+            var user = await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x => 
             x.UserName == loginDTO.Username);
 
             if(user == null) 
@@ -72,7 +74,8 @@ namespace API.Controllers
             return new UserDTO
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
